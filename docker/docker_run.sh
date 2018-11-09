0791f57ca43d1237fe3c2392b9349bcd33733c94
pglotfel <pglotfel@gmail.com>
2018-11-09 14:51:23 -0500

    changes

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,1 +1,6 @@
-docker run --rm -ti robotarium:video_recorder
+docker run --rm -ti \
+	--env="DISPLAY" \
+	-v /home/robotarium-workstation/git/video_recorder/record_video.py:/record_video.py \
+	-v /tmp/.X11-unix/:/tmp/.X11-unix/:rw \
+	--net=host \
+robotarium:video_recorder
